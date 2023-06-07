f0b3a6b9ea44980841fd997b8bcfb85aa9650dad
DiegoCamilli <camillidiego@yahoo.com>
2023-06-07 07:54:33 -0700

    update file to finish routes based on comments

--- routes/api/catergory-routes.js
+++ routes/api/catergory-routes.js
@@ -1,28 +1,61 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router()
+const { Category, Product } = require("../../models")
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // find all categories
   // be sure to include its associated Products
-});
+  Category.findAll({
+    include: [Product],
+  })
+    .then((data) => res.json(data).status(200))
+    .catch((err) => res.json(err).status(400))
+})
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-});
+  Category.findOne({
+    where: {
+      // grab by id
+      id: req.params.id,
+    },
+    include: [Product],
+  })
+    .then((data) => res.json(data).status(200))
+    .catch((err) => res.json(err).status(400))
+})
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new category
-});
+  Category.create(req.body)
+    .then((data) => res.json(data).status(200))
+    .catch((err) => res.json(err).status(400))
+})
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a category by its `id` value
-});
+  Category.update(req.body, {
+    where: {
+      // use id to update
+      id: req.params.id,
+    },
+  })
+    .then((data) => res.json(data).status(200))
+    .catch((err) => res.json(err).status(400))
+})
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
-});
+  Category.destroy({
+    where: {
+      // use id to find and delete
+      id: req.params.id,
+    },
+  })
+    .then((data) => res.json(data).status(200))
+    .catch((err) => res.json(err).status(400))
+})
 
-module.exports = router;
+module.exports = router
